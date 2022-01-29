be46ede28c6ddf8b0919fabc5e3e7fd4f2dab926
DohyeonPark <dodo4114@naver.com>
2022-01-30 01:46:59 +0900

    Edit smth

--- src/app/components/Styled.ts
+++ src/app/components/Styled.ts
@@ -1,5 +1,4 @@
 import styled from 'styled-components/macro';
-
 export const P = styled.p`
   font-size: 1rem;
   line-height: 1.5;
@@ -11,4 +10,32 @@ export const Title = styled.h1`
   line-height: 1.5;
   color: black;
   margin: 0.625rem 0 1.5rem 0;
+`;
+
+export const SubTitle = styled.h2`
+  font-size: 3rem;
+  line-height: 1.5;
+  color: gray;
+  margin: 0.625rem 0 1.5rem 0;
+`;
+export const SemiTitle = styled.h2`
+  font-size: 2rem;
+  line-height: 1.5;
+  color: black;
+  margin: 0.625rem 0 1.5rem 0;
+`;
+
+export const Highlight = styled.span`
+  background-image: linear-gradient(
+    transparent ${p => (p['top'] ? p['top'] : '20%')},
+    ${p => (p.color ? p.color : '#f6bd60')}
+      ${p => (p['top'] ? p['top'] : '20%')}
+      ${p => (p['bottom'] ? p['bottom'] : '45%')},
+    transparent ${p => (p['bottom'] ? p['bottom'] : '45%')}
+  );
+  transition: background-position 120ms ease-in-out 0s,
+    padding 120ms ease-in-out 0s;
+  background-size: 100% 200%;
+  background-position: initial;
+  background-color: initial;
 `;
--- src/app/pages/HomePage/index.tsx
+++ src/app/pages/HomePage/index.tsx
@@ -1,16 +1,34 @@
-import { P, Title } from 'app/components/Styled';
-import * as React from 'react';
+import {
+  SubTitle,
+  Title,
+  Highlight,
+  SemiTitle,
+  P,
+} from 'app/components/Styled';
 import { Helmet } from 'react-helmet-async';
 
 export function HomePage() {
+  const todoList = [
+    "What projects I've done",
+    'Contacts : e-mail, open-kakao, facebook',
+    'Personas : DODO, Do-veloper, Dohyeon Park',
+    'Thoughts : Blog, Object for my life',
+    'Make it 3D/VR + Web3?',
+  ];
   return (
     <>
       <Helmet>
         <title>HomePage</title>
         <meta name="description" content="A Boilerplate application homepage" />
       </Helmet>
-      <Title>Hi there 👋 This is DODO</Title>
-      <P>This page is very first version of my homepage</P>
+      <Title>
+        Hi there 👋 This is <Highlight>DODO</Highlight>
+      </Title>
+      <SubTitle>This page is the very first version of my homepage</SubTitle>
+      <SemiTitle>TODO</SemiTitle>
+      {todoList.map(value => (
+        <P key={value}>- {value}</P>
+      ))}
     </>
   );
 }
